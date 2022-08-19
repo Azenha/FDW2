28d10f1d3f92abfe58cbac6db0d7ff9d838721e0
Leonardo Azenha <azenha.leonardo@gmail.com>
2022-08-18 21:44:00 -0300

    update final aula 1

--- Atividade_1/app.js
+++ Atividade_1/app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let listaProdutos = [];
+let geradorId = 1;
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -11,19 +17,20 @@ app.get('/produtos', (req, res) => {
 })
 
 app.get('/produtos/:id', (req, res) => {
-    res.send('Produto! ID')
+    res.send('Buscando o produto!')
 })
 
 app.post('/produtos', (req, res) => {
-    res.send('Produto POST!')
+    //res.send('Cadastro de produto!')
+    res.json(req.body)
 })
 
 app.put('/produtos/:id', function (req, res) {
-    res.send('Produto PUT!');
+    res.send('Atualizando o produto!');
   });
 
 app.delete('/produtos/:id', function (req, res) {
-    res.send('Produto DELETE!');
+    res.send('Deletando o produto!');
   });
 
 app.listen(port, () => {
